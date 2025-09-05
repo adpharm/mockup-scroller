0ee0b14ebd4e445966cd419e20fd81f5bf54c8a5
ben honda <bhonda89@gmail.com>
2025-09-05 11:25:54 -0400

    Add simple test scripts for local/rethink-aug28 folder
    - test-rethink.sh: Full processing with all outputs
    - test-rethink-fast.sh: GIF only mode for faster testing

---
+++ test-rethink-fast.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Fast mode - GIF only, no segments
+# Use when you just need the animations
+
+bun run dev --input "./local/rethink-aug28" --out "./out/rethink-fast" --no-segments
---
+++ test-rethink.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Process all PNGs in local/rethink-aug28 folder
+# Generates: GIF animations + framed segments + screen segments
+
+bun run dev --input "./local/rethink-aug28" --out "./out/rethink-test"
